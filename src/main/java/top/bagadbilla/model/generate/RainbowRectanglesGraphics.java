a5a957332601e612a6c21ffd3de31be4418e2822
Ramandeep Singh <ramandeep89@gmail.com>
2024-06-05 15:01:33 +0530

    Fixed misfills

--- src/main/java/top/bagadbilla/model/generate/RainbowRectanglesGraphics.java
+++ src/main/java/top/bagadbilla/model/generate/RainbowRectanglesGraphics.java
@@ -15,7 +15,7 @@ public class RainbowRectanglesGraphics extends BaseGraphics {
     }
 
     public void generate() {
-//        fill(Color.WHITE);
+        fill(Color.WHITE);
         double size = Math.random() * 60 + 10;
         double baseHueOffset = Math.random() * 360;
         int hueMode = (int) Math.floor(Math.random() * 4);
@@ -27,14 +27,14 @@ public class RainbowRectanglesGraphics extends BaseGraphics {
     }
 
     private void drawRectangle(double size, double x, double y, double baseHueOffset, int hueMode) {
+        g.translate(x, y);
         double widthModification = Math.random() + 1;
         double heightModification = Math.random() * 3 + 1;
         double width = size * widthModification;
         double height = size * heightModification;
         Rectangle2D rect = new Rectangle2D.Double(-width / 2, -height / 2, width, height);
-        AffineTransform at = new AffineTransform();
-        at.rotate((Math.random() - 0.5) * 0.1);
-        at.translate(x, y);
+        Shape shape = AffineTransform.getRotateInstance((Math.random() - 0.5) * 0.1)
+                .createTransformedShape(rect);
         double randomHueOffset = Math.random() * 20;
         double p = 0;
         if (hueMode == 0) {
@@ -50,7 +50,8 @@ public class RainbowRectanglesGraphics extends BaseGraphics {
         double l = Math.random() * 20 + 40;
         Color color = HSLColor.toRGB((float) hue, 80F, (float) l, 0.7F);
         g.setColor(color);
-        g.fill(at.createTransformedShape(rect));
+        g.fill(shape);
+        g.translate(-x, -y);
     }
 
 }
