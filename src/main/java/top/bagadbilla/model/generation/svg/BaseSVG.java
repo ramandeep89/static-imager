ecaa14f5f4676cab580277ce995a020ff4044522
Ramandeep Singh <ramandeep89@gmail.com>
2025-04-10 00:10:22 +0530

    Added Geopatterns

--- src/main/java/top/bagadbilla/model/generation/svg/BaseSVG.java
+++ src/main/java/top/bagadbilla/model/generation/svg/BaseSVG.java
@@ -1,17 +1,5 @@
 package top.bagadbilla.model.generation.svg;
 
-import java.io.StringWriter;
-import java.util.List;
-import java.util.Map;
-import java.util.Stack;
-import java.util.stream.Collectors;
-
-import javax.xml.transform.OutputKeys;
-import javax.xml.transform.Transformer;
-import javax.xml.transform.TransformerFactory;
-import javax.xml.transform.dom.DOMSource;
-import javax.xml.transform.stream.StreamResult;
-
 import org.apache.batik.anim.dom.SVGDOMImplementation;
 import org.jetbrains.annotations.NotNull;
 import org.w3c.dom.DOMImplementation;
@@ -19,14 +7,25 @@ import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import top.bagadbilla.model.generation.BaseGeneration;
 
+import javax.xml.transform.OutputKeys;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+import java.io.StringWriter;
+import java.util.List;
+import java.util.Map;
+import java.util.Stack;
+import java.util.stream.Collectors;
+
 public abstract class BaseSVG extends BaseGeneration {
 
     protected final Document document;
-    protected final int width, height;
     protected final Element svg;
     protected final Stack<Element> context = new Stack<>();
+    protected final int width, height;
 
-    protected BaseSVG(int width, int height) {
+    public BaseSVG(int width, int height) {
         super(width, height);
         DOMImplementation impl = SVGDOMImplementation.getDOMImplementation();
         this.document = impl.createDocument(SVGDOMImplementation.SVG_NAMESPACE_URI, "svg", null);
@@ -38,8 +37,7 @@ public abstract class BaseSVG extends BaseGeneration {
     }
 
     protected final Element currentContext() {
-        if (!context.isEmpty())
-            return context.peek();
+        if (!context.isEmpty()) return context.peek();
         else return svg;
     }
 
@@ -47,66 +45,91 @@ public abstract class BaseSVG extends BaseGeneration {
         return context.pop();
     }
 
+    protected final void add(Element e) {
+        context.push(e);
+    }
+
     protected final Element currentNode() {
         Element context = this.currentContext();
-        if (context.hasChildNodes())
-            return (Element) context.getLastChild();
+        if (context.hasChildNodes()) return (Element) context.getLastChild();
         else return context;
     }
 
-    protected final void transform(@NotNull Map<String, List<String>> transformations) {
-        this.currentNode().setAttribute("transform", transformations.entrySet().stream().map(
-                        e -> e.getKey() + "(" + String.join(",", e.getValue()) + ")"
-                ).collect(Collectors.joining(" "))
-        );
+    protected final BaseSVG transform(@NotNull Map<String, List<Object>> transformations) {
+        this.currentNode().setAttribute("transform", transformations.entrySet().stream().map(e -> e.getKey() + "(" + e.getValue().stream().map(Object::toString).collect(Collectors.joining(",")) + ")").collect(Collectors.joining(" ")));
+        return this;
     }
 
-    protected final void setAttributes(Element el, @NotNull Map<String, String> attrs) {
-        attrs.forEach(el::setAttribute);
+    protected final BaseSVG setAttributes(Element el, @NotNull Map<String, Object> attrs) {
+        attrs.forEach((k, v) -> el.setAttribute(k, String.valueOf(v)));
+        return this;
     }
 
-    protected final void rect(int x, int y, int width, int height, @NotNull Map<String, String> args) {
+    protected final BaseSVG rect(double x, double y, String width, String height, @NotNull Map<String, Object> args) {
         Element rect = document.createElement("rect");
         this.currentContext().appendChild(rect);
-        this.setAttributes(rect, Map.of(
-                "x", String.valueOf(x),
-                "y", String.valueOf(y),
-                "width", String.valueOf(width),
-                "height", String.valueOf(height)
-        ));
+        this.setAttributes(rect, Map.of("x", String.valueOf(x), "y", String.valueOf(y), "width", width, "height", height));
         this.setAttributes(rect, args);
+        return this;
     }
 
-    protected final void circle(int cx, int cy, int r, @NotNull Map<String, String> args) {
+    protected final BaseSVG rect(double x, double y, double width, double height, @NotNull Map<String, Object> args) {
+        Element rect = document.createElement("rect");
+        this.currentContext().appendChild(rect);
+        this.setAttributes(rect, Map.of("x", String.valueOf(x), "y", String.valueOf(y), "width", String.valueOf(width), "height", String.valueOf(height)));
+        this.setAttributes(rect, args);
+        return this;
+    }
+
+    protected final BaseSVG rect(double[] params, @NotNull Map<String, Object> args) {
+        if (params.length != 4) throw new IllegalArgumentException("strings.length != 4");
+        else return rect(params[0], params[1], params[2], params[3], args);
+    }
+
+    protected final BaseSVG rect(List<double[]> lists, @NotNull Map<String, Object> args) {
+        for (double[] params : lists) {
+            rect(params, args);
+        }
+        return this;
+    }
+
+    protected final BaseSVG circle(double cx, double cy, double r, @NotNull Map<String, Object> args) {
         Element circle = document.createElement("circle");
         this.currentContext().appendChild(circle);
-        this.setAttributes(circle, Map.of(
-                "cx", String.valueOf(cx),
-                "cy", String.valueOf(cy),
-                "r", String.valueOf(r)
-        ));
+        this.setAttributes(circle, Map.of("cx", String.valueOf(cx), "cy", String.valueOf(cy), "r", String.valueOf(r)));
         this.setAttributes(circle, args);
+        return this;
     }
 
-    protected final void path(String str, @NotNull Map<String, String> args) {
+    protected final BaseSVG path(String str, @NotNull Map<String, Object> args) {
         Element path = document.createElement("path");
         this.currentContext().appendChild(path);
         this.setAttributes(path, Map.of("d", str));
         this.setAttributes(path, args);
+        return this;
     }
 
-    protected final void polyline(String str, @NotNull Map<String, String> args) {
+    protected final BaseSVG polyline(String str, @NotNull Map<String, Object> args) {
         Element polyline = document.createElement("polyline");
         this.currentContext().appendChild(polyline);
         this.setAttributes(polyline, Map.of("points", str));
         this.setAttributes(polyline, args);
+        return this;
     }
 
-    protected final void group(@NotNull Map<String, String> args) {
+    protected final BaseSVG polyline(List<String> strings, @NotNull Map<String, Object> args) {
+        for (String str : strings) {
+            polyline(str, args);
+        }
+        return this;
+    }
+
+    protected final BaseSVG group(@NotNull Map<String, Object> args) {
         Element group = document.createElement("g");
         this.currentContext().appendChild(group);
         this.context.push(group);
         this.setAttributes(group, args);
+        return this;
     }
 
     public abstract BaseSVG generateSVG();
